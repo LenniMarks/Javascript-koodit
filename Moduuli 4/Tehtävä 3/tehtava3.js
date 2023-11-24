8629631b9951d678ed8c2c5fb1728c16b82492d0
LenniMarks <123360910+LenniMarks@users.noreply.github.com>
2023-11-24 14:28:01 +0200

    moduuli 4 tehtävät 1-4

---
+++ Moduuli 4/Tehtävä 3/tehtava3.js
@@ -0,0 +1,44 @@
+'use strict';
+const movieForm = document.querySelector("form")
+movieForm.addEventListener('submit', async function(evt) {
+    evt.preventDefault();
+    const query = document.getElementById("query").value;
+    try {                                             // error handling: try/catch/finally
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);    // starting data download, fetch returns a promise which contains an object of type 'response'
+        const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function 
+                             
+        const div = document.getElementById("results");
+        div.innerHTML = ''
+
+        jsonData.forEach(tvshow => {
+        
+        const article = document.createElement("article");
+    
+        const title = document.createElement("h2");
+        title.textContent = tvshow.show.name;
+                      
+        const url = document.createElement("a");
+        url.href = tvshow.show.url;
+        url.textContent = "Details";
+        url.target = '_blank';
+                    
+        const articleImage = document.createElement("img");
+        articleImage.src = tvshow.show.image?.medium;
+        articleImage.alt = tvshow.show.name;
+                      
+        const summary = document.createElement("div");
+        summary.innerHTML = tvshow.show.summary;
+                      
+        div.appendChild(article);
+                                    
+        article.appendChild(title);
+        article.appendChild(url);
+        article.appendChild(articleImage);
+        article.appendChild(summary);
+        });
+
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
